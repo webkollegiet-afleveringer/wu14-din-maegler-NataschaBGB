3c2eb8f4bf775134f44e5b351d0887cadff11558
NataschaBGB <nannabjerning@hotmail.com>
2026-03-23 13:22:11 +0100

    add: header and footer
    added components and basic content in header

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.sass';
+
+export default function Footer() {
+
+    return (
+        <footer>
+            <p>&copy; 2024 Din Mægler. All rights reserved.</p>
+        </footer>
+    )
+
+}
