2853a6b046afe051a9e9d7ea4a295a6093773c9f
NataschaBGB <nannabjerning@hotmail.com>
2026-03-26 12:11:45 +0100

    add: Houses page and House details page
    Will add content later

--- src/pages/House.jsx
+++ src/pages/House.jsx
@@ -1,1 +1,27 @@
 // house details page when clicking on a house in the list on Houses.jsx
+
+export default function House() {
+
+    // for loader:
+    // export default async function houseDetailsLoader({ params }) {
+    
+    // const houseId = params.houseId;
+
+    // const response = await fetch(`https://dinmaegler.onrender.com/homes/${houseId}`);
+    // if (!response.ok) {
+    //         throw new Error('Failed to fetch house details');
+    //     }
+    //     const house = await response.json();
+    //     return house;
+    // }
+
+    // const { houseId } = useParams();
+
+
+    return (
+        <section className="house-page">
+            <h1>House details page</h1>
+        </section>
+    )
+
+}
---
+++ src/pages/Houses.jsx
@@ -0,0 +1,9 @@
+export default function Houses() {
+
+    return (
+        <section className="houses-page">
+            <h1>Houses</h1>
+        </section>
+    )
+    
+}
