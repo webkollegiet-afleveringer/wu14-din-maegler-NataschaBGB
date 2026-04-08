fd75d44256e19d8caca430ec198738dbc3b62073
NataschaBGB <nannabjerning@hotmail.com>
2026-04-08 13:19:03 +0200

    add: Error page layout
    Finished layout for Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,28 @@
+import { Link } from "react-router";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+
 export default function Error() {
     return (
-        <main>
-            <h1>404 - Page Not Found</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
-        </main>
+        <section className="error-page">
+
+            <Header />
+
+            <main>
+                
+                <h1>Hov!</h1>
+            
+                <h2>Du er havnet på en side som ikke findes!</h2>
+                
+                <p>Det er vi kede af! Vi har sendt en besked af sted til vores internetbureau og bedt dem om, at se på fejlen.</p>
+
+                <Link to="/">Tilbage til forsiden</Link>
+                
+            </main>
+
+            <Footer />
+
+        </section>
+        
     )
 }
