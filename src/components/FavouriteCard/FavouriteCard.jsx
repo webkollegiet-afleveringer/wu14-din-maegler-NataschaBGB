c68dcf88bdd1d529a6ee6b98b4d8bca55e75b777
NataschaBGB <nannabjerning@hotmail.com>
2026-04-17 08:40:47 +0200

    fix: link to home details from fav and styling

--- src/components/FavouriteCard/FavouriteCard.jsx
+++ src/components/FavouriteCard/FavouriteCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import './FavouriteCard.sass';
 
 
@@ -23,17 +24,19 @@ import './FavouriteCard.sass';
 export default function FavouriteCard({ home, removeBookmark }) {
     
     return (
-        <article id={home.id ? home.id : ''} className='favourite-card'>
+        <article key={home.id ? home.id : ''} className='favourite-card'>
 
-            <img src={home.image ? home.image : '/default-image.jpg'} alt={home.title ? home.title : 'Titel ikke tilgængelig' } />
+            <Link to={`/houses/${home.id ? home.id : ''}`} className='favourite-card__link'>
 
-            <section className='favourite-card__header'>
-                <h2 className='favourite-card__title'>{home.title ? home.title : 'Titel ikke tilgængelig'}</h2>
-                <p className='favourite-card__location'>{home.postalcode ? home.postalcode : 'Postnummer ikke tilgængeligt'} {home.city ? home.city : 'By ikke tilgængelig'}</p>
+                <img src={home.image ? home.image : '/default-image.jpg'} alt={home.title ? home.title : 'Titel ikke tilgængelig' } />
 
-                <p className='favourite-card__type'><span>{home.type ? home.type : 'Type ikke tilgængelig'}</span> • Ejerudgift: {home.cost ? home.cost?.toLocaleString('da-DK') : null} kr.</p>
-            </section>
-            
+                <section className='favourite-card__header'>
+                    <h2 className='favourite-card__title'>{home.title ? home.title : 'Titel ikke tilgængelig'}</h2>
+                    <p className='favourite-card__location'>{home.postalcode ? home.postalcode : 'Postnummer ikke tilgængeligt'} {home.city ? home.city : 'By ikke tilgængelig'}</p>
+
+                    <p className='favourite-card__type'><span>{home.type ? home.type : 'Type ikke tilgængelig'}</span> • Ejerudgift: {home.cost ? home.cost?.toLocaleString('da-DK') : null} kr.</p>
+                </section>
+            </Link>
 
             {home.energylabel ? (
                 <div className={`favourite-card__energylabel-value ${home.energylabel === 'A' ? 'a-label' : home.energylabel === 'B' ? 'b-label' : home.energylabel === 'C' ? 'c-label' :
